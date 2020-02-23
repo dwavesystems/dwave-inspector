dwavesystems/dwave-inspector

commit ba467de963c82e779ff509be861251d19b6c3554

Use generalized number of occurrences on client's Future
This ensures `answer_mode='raw'` is handled properly, as in that case
the response from SAPI does not contain 'num_occurrences' field.

Closes #23.

dwave/inspector/adapters.py
@@ -350,7 +350,7 @@ def from_qmi_response(problem, response, embedding_context=None, warnings=None,
 
     solutions = list(map(itemgetter(*active_variables), response['solutions']))
     energies = response['energies']
-    num_occurrences = response['num_occurrences']
+    num_occurrences = response.occurrences
     num_variables = solver.num_qubits
     timing = response.timing
 
@@ -448,7 +448,7 @@ def from_bqm_response(bqm, embedding_context, response, warnings=None,
 
     solutions = list(map(itemgetter(*active_variables), response['solutions']))
     energies = response['energies']
-    num_occurrences = response['num_occurrences']
+    num_occurrences = response.occurrences
     num_variables = solver.num_qubits
     timing = response.timing
 

commit e9c23b7f43fc68ef5abb5b214937b452708a21bc

Fix regression in no-data-capture mode introduced in ba467de96

dwave/inspector/adapters.py
@@ -141,6 +141,7 @@ def _expand_params(solver, params=None, timing=None):
 
     # set each parameter individually because defaults are not necessarily
     # constant; they can depend on one or more other parameters
+    # TODO: cast to primary types for safe JSON serialization
     return {
         "anneal_offsets": params.get("anneal_offsets"),
         "anneal_schedule": anneal_schedule,
@@ -370,7 +371,7 @@ def from_qmi_response(problem, response, embedding_context=None, warnings=None,
 
     # try to reconstruct sampling params
     if params is None:
-        params = {'num_reads': sum(num_occurrences)}
+        params = {'num_reads': int(sum(num_occurrences))}
 
     # expand with defaults
     params = _expand_params(solver, params, timing)
@@ -483,7 +484,7 @@ def from_bqm_response(bqm, embedding_context, response, warnings=None,
 
     # try to reconstruct sampling params
     if params is None:
-        params = {'num_reads': sum(num_occurrences)}
+        params = {'num_reads': int(sum(num_occurrences))}
 
     # expand with defaults
     params = _expand_params(solver, params, timing)
@@ -647,7 +648,7 @@ def from_bqm_sampleset(bqm, sampleset, sampler, embedding_context=None,
 
     # try to reconstruct sampling params
     if params is None:
-        params = {'num_reads': sum(num_occurrences)}
+        params = {'num_reads': int(sum(num_occurrences))}
 
     # expand with defaults
     params = _expand_params(solver, params, timing)