79d98fdad795ed6f2e8167208dad227fb1269485
Radomir Stevanovic <radomir@dwavesys.com>
2020-02-24 05:42:37 -0800

    Add missing packages in 'test' extras

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ install_requires = [
 
 # Package extras requirements
 extras_require = {
-    'test': ['coverage', 'mock'],
+    'test': ['coverage', 'mock', 'vcrpy'],
 
     # backports
     ':python_version < "3.7"': ['importlib_resources']
