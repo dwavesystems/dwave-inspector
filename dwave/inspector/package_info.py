90f93954c2097bcec7b24d39245f9d7c02101295
Radomir Stevanovic <radomir@dwavesys.com>
2020-03-26 13:49:50 -0700

    Release 0.1.5
    Changes since 0.1.4:
    
    New Features
    ---
    
    - Documentation moved from Readme to ReadTheDocs

--- dwave/inspector/package_info.py
+++ dwave/inspector/package_info.py
@@ -20,7 +20,7 @@ __all__ = [
 
 __package_name__ = 'dwave-inspector'
 __title__ = 'D-Wave Problem Inspector'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __author__ = 'D-Wave Systems Inc.'
 __author_email__ = 'radomir@dwavesys.com'
 __description__ = 'D-Wave Problem Inspector tool'
