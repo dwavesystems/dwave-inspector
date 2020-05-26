cac4d809799f782b09740d0a013a7596d9736d62
Radomir Stevanovic <radomir@dwavesys.com>
2020-05-26 10:48:22 -0700

    Remove '__future__.absolute_import' dep (mandatory in py3+)

--- dwave/inspector/__init__.py
+++ dwave/inspector/__init__.py
@@ -12,11 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import absolute_import
-
 import os
-import logging
 import enum
+import logging
 
 from dwave.cloud.utils import set_loglevel
 
