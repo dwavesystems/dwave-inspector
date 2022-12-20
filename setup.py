689a60f860f018d60478522a70d6993ab8f5348b
Radomir Stevanovic <radomir@dwavesys.com>
2022-12-20 06:49:14 -0800

    Update dwave-inspectorapp to add support for relative app paths
    Also include improvements in error handling and display.
    
    Fix #146.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [
     'dwave-cloud-client>=0.8.3',
     'Flask>=1.1.1',
     'numpy',
-    # dwave-inspectorapp==0.3.0
+    # dwave-inspectorapp==0.3.1
 ]
 
 # Package extras requirements
