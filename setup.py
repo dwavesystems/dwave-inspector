14e0aba865735dc864c6d1c1248df48730ee13a2
Radomir Stevanovic <radomir@dwavesys.com>
2023-08-24 04:15:35 -0700

    Drop Python 3.7 tests and classifiers

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ classifiers = [
     'Operating System :: OS Independent',
     'Development Status :: 3 - Alpha',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
@@ -45,7 +44,7 @@ classifiers = [
 
 packages = ['dwave', 'dwave.inspector']
 
-python_requires = '>=3.7'
+python_requires = '>=3.8'
 
 setup(
     name=package_info['__package_name__'],
