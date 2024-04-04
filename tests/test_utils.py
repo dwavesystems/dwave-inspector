0281544f72d1970caa51721842051de5a66e25fa
Radomir Stevanovic <radomir@dwavesys.com>
2024-04-04 15:16:12 -0700

    Verify numpy types serialization with OrJSONProvider

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,12 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import json
 import unittest
 
 from uuid import uuid4
 from urllib.parse import ParseResult, urlparse, urljoin
 
-from dwave.inspector.utils import itemsgetter, annotated, update_url_from
+import numpy
+from parameterized import parameterized
+
+from dwave.inspector.utils import itemsgetter, annotated, update_url_from, OrJSONProvider
 
 
 class TestItemsgetter(unittest.TestCase):
@@ -103,3 +107,52 @@ class TestUrlRewrite(unittest.TestCase):
         src, dst = 'http://localhost:9000/local', 'https://example.com/external/'
         url = update_url_from(src, dst, path=lambda src, dst: urljoin(dst.path, src.path.lstrip('/')))
         self.assertEqual(url, 'https://example.com/external/local')
+
+
+# adapted from dwave-hybrid utils
+# (https://github.com/dwavesystems/dwave-hybrid/blob/b9025b5bb3d88dce98ec70e28cfdb25400a10e4a/hybrid/utils.py#L43-L61)
+class TestOrJSONProvider(unittest.TestCase):
+
+    @parameterized.expand([
+        (numpy.bool_(1), True), (numpy.bool8(1), True),
+        (numpy.byte(1), 1), (numpy.int8(1), 1),
+        (numpy.ubyte(1), 1), (numpy.uint8(1), 1),
+        (numpy.short(1), 1), (numpy.int16(1), 1),
+        (numpy.ushort(1), 1), (numpy.uint16(1), 1),
+        (numpy.intc(1), 1), (numpy.int32(1), 1),
+        (numpy.uintc(1), 1), (numpy.uint32(1), 1),
+        (numpy.int_(1), 1), (numpy.int32(1), 1),
+        (numpy.uint(1), 1), (numpy.uint32(1), 1),
+        (numpy.int64(1), 1),
+        (numpy.uint64(1), 1),
+        (numpy.half(1.0), 1.0), (numpy.float16(1.0), 1.0),
+        (numpy.single(1.0), 1.0), (numpy.float32(1.0), 1.0),
+        (numpy.double(1.0), 1.0), (numpy.float64(1.0), 1.0),
+        # note: orjson does not currently support: longlong, ulonglong, longdouble
+        # see https://github.com/ijl/orjson/issues/469
+    ])
+    def test_numpy_primary_type_encode(self, np_val, py_val):
+        provider = OrJSONProvider(app=self)
+        self.assertEqual(
+            json.dumps(py_val, separators=(',', ':')).encode(),
+            provider.dumps(np_val)
+        )
+
+    @parameterized.expand([
+        (numpy.array([1, 2, 3], dtype=int), [1, 2, 3]),
+        (numpy.array([[1], [2], [3]], dtype=float), [[1.0], [2.0], [3.0]]),
+        (numpy.zeros((2, 2), dtype=bool), [[False, False], [False, False]]),
+        # note: recarrays are not support by orjson. if needed, we can add support via default callback
+        # (numpy.array([('Rex', 9, 81.0), ('Fido', 3, 27.0)],
+        #              dtype=[('name', 'U10'), ('age', 'i4'), ('weight', 'f4')]),
+        #  [['Rex', 9, 81.0], ['Fido', 3, 27.0]]),
+        # (numpy.rec.array([(1, 2., 'Hello'), (2, 3., "World")],
+        #                  dtype=[('foo', 'i4'), ('bar', 'f4'), ('baz', 'U10')]),
+        #  [[1, 2.0, "Hello"], [2, 3.0, "World"]])
+    ])
+    def test_numpy_array_encode(self, np_val, py_val):
+        provider = OrJSONProvider(app=self)
+        self.assertEqual(
+            json.dumps(py_val, separators=(',', ':')).encode(),
+            provider.dumps(np_val)
+        )
