1ff70ae17d236f59d71218265f246b058b48700d
Radomir Stevanovic <radomir@dwavesys.com>
2024-06-13 07:29:53 -0700

    Release 0.5.0.post0
    Fixes cloud-client upper bound.

--- dwave/inspector/package_info.py
+++ dwave/inspector/package_info.py
@@ -20,7 +20,7 @@ __all__ = [
 
 __package_name__ = 'dwave-inspector'
 __title__ = 'D-Wave Problem Inspector'
-__version__ = '0.5.0'
+__version__ = '0.5.0.post0'
 __author__ = 'D-Wave Systems Inc.'
 __author_email__ = 'radomir@dwavesys.com'
 __description__ = 'D-Wave Problem Inspector tool'
